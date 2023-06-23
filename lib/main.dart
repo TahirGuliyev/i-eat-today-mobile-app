725df69a76e9a9f939b6ac987f326b6f5d813271
Tahir Guliyev <tahirguliyev95@gmail.com>
2023-06-23 19:56:57 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,21 @@ class _FoodPageState extends State<FoodPage> {
   int sorbaNo = 1;
   int yemekNo = 1;
   int sirniyyatNo = 1;
+  List<String> sorbaAdlari = ['Mərci', 'Tarhana', 'Toyuq', 'Toy', 'Dovğa'];
+  List<String> yemekAdlari = [
+    'Qarnıyarıq',
+    'Mantı',
+    'Kuru Fasulye',
+    'İçli küftə',
+    'Balıq'
+  ];
+  List<String> sirniyyatAdlari = [
+    'Kadayıf',
+    'Türk paxlavası',
+    'Pudinq',
+    'Kazandibi',
+    'Dondurma'
+  ];
   void reloadFoods() {
     sorbaNo = Random().nextInt(5) + 1;
     sirniyyatNo = Random().nextInt(5) + 1;
@@ -62,6 +77,17 @@ class _FoodPageState extends State<FoodPage> {
                   child: Image.asset('assets/images/sorba_$sorbaNo.jpg')),
             ),
           ),
+          Text(
+            sorbaAdlari[sorbaNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            ),
+          ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(12.0),
@@ -73,6 +99,17 @@ class _FoodPageState extends State<FoodPage> {
                     setState(reloadFoods);
                   },
                   child: Image.asset('assets/images/yemek_$yemekNo.jpg')),
+            ),
+          ),
+          Text(
+            yemekAdlari[yemekNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
             ),
           ),
           Expanded(
@@ -87,6 +124,17 @@ class _FoodPageState extends State<FoodPage> {
                   },
                   child:
                       Image.asset('assets/images/sirniyyat_$sirniyyatNo.jpg')),
+            ),
+          ),
+          Text(
+            sirniyyatAdlari[sirniyyatNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
             ),
           ),
         ],
